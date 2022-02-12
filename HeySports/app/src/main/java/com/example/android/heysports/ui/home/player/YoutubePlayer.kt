de7d55a22afd48c40f22c76638fe2d2d62f2f78f
hello2727 <jihye7369@gmail.com>
2022-02-13 01:23:25 +0900

    create youtube class

--- HeySports/app/src/main/java/com/example/android/heysports/ui/home/player/YoutubePlayer.kt
+++
@@ -1,8 +0,0 @@
-package com.example.android.heysports.ui.home.player
-
-/**
- * Created by Jihye Noh
- * Date: 2022-02-12
- */
-class YoutubePlayer {
-}
---
+++ HeySports/app/src/main/java/com/example/android/heysports/util/player/YoutubeVideoRetriever.kt
@@ -0,0 +1,12 @@
+package com.example.android.heysports.util.player
+
+/**
+ * Created by Jihye Noh
+ * Date: 2022-02-13
+ */
+class YoutubeVideoRetriever {
+
+    fun retrieve(videoId: String){
+
+    }
+}
