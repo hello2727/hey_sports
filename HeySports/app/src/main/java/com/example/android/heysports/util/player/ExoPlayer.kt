18504484e9beb204d64ded17f1659a08cf8169b3
hello2727 <jihye7369@gmail.com>
2022-02-24 01:09:03 +0900

    player build 선언

--- HeySports/app/src/main/java/com/example/android/heysports/util/player/ExoPlayer.kt
+++ HeySports/app/src/main/java/com/example/android/heysports/util/player/ExoPlayer.kt
@@ -1,8 +1,13 @@
 package com.example.android.heysports.util.player
 
 import androidx.appcompat.app.AppCompatActivity
+import com.example.android.heysports.network.HOME_INTRODUCTION_VIDEO_URL
 import com.google.android.exoplayer2.ExoPlayer
+import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.Player
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
+import com.google.android.exoplayer2.util.MimeTypes
+import com.google.android.exoplayer2.util.Util
 import timber.log.Timber
 
 /**
@@ -19,10 +24,25 @@ class ExoPlayer : AppCompatActivity() {
 
     override fun onStart() {
         super.onStart()
+        if (Util.SDK_INT > 23) {
+            initializePlayer()
+        }
     }
 
     private fun initializePlayer() {
+        val trackSelector = DefaultTrackSelector(this).apply {
+            setParameters(buildUponParameters().setMaxVideoSizeSd())
+        }
+        player = ExoPlayer.Builder(this)
+            .setTrackSelector(trackSelector)
+            .build()
+            .also { exoPlayer ->
 
+                val mediaItem = MediaItem.Builder()
+                    .setUri(HOME_INTRODUCTION_VIDEO_URL)
+                    .setMimeType(MimeTypes.APPLICATION_MPD)
+                    .build()
+            }
     }
 }
 
