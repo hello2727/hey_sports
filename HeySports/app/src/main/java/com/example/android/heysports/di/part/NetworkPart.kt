09811cb5eecc26c7a717fa291a8c7f6f469f690e
hello2727 <jihye7369@gmail.com>
2022-02-28 00:53:44 +0900

    gson, retrofit client 추가

--- HeySports/app/src/main/java/com/example/android/heysports/di/part/NetworkPart.kt
+++ HeySports/app/src/main/java/com/example/android/heysports/di/part/NetworkPart.kt
@@ -4,6 +4,8 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import javax.inject.Singleton
 
 /**
@@ -13,9 +15,17 @@ import javax.inject.Singleton
 @Module
 @InstallIn(SingletonComponent::class)
 object NetworkPart {
-//    @Provides
-//    @Singleton
-//    fun getRetrofit(){
-//
-//    }
+    @Provides
+    @Singleton
+    fun getRetrofitClient(): Retrofit = Retrofit.Builder()
+        .baseUrl(YOUTUBE_API)
+        .addConverterFactory(getGson())
+        .build()
+
+    @Provides
+    @Singleton
+    fun getGson(): GsonConverterFactory = GsonConverterFactory.create()
 }
+
+private val YOUTUBE_API =
+    "https://www.googleapis.com/youtube/v3/videos?id=7lCDEYXw3mM&key=AIzaSyDGqNg6oN-YBQr2DKAX1j4Om9L5i5r1Xvk&part=snippet,contentDetails,statistics,status"
