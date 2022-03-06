cf14a5617502b9c0c5616eace7a1720e5246b022
hello2727 <jihye7369@gmail.com>
2022-03-06 18:59:58 +0900

    url 변경

--- HeySports/app/src/main/java/com/example/android/heysports/network/Url.kt
+++ HeySports/app/src/main/java/com/example/android/heysports/network/Url.kt
@@ -5,4 +5,4 @@ package com.example.android.heysports.network
  * Date: 2022-02-08
  */
 const val HOME_INTRODUCTION_VIDEO_URL = "https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
-const val videoUrl = "https://www.youtube.com/watch?v=Kj2HMwXKoBw"
+const val videoUrl = "https://www.youtube.com/watch?v=aPYldRoEbZs"
