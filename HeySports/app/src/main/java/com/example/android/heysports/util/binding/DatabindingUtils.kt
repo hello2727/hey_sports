605096a6b7ae4e68167a537ba374b4e3cead612e
hello2727 <jihye7369@gmail.com>
2022-03-26 15:40:59 +0900

    경기일정 리스트 추가(올림픽, 월드컵)

--- HeySports/app/src/main/java/com/example/android/heysports/util/binding/DatabindingUtils.kt
+++ HeySports/app/src/main/java/com/example/android/heysports/util/binding/DatabindingUtils.kt
@@ -1,7 +1,10 @@
 package com.example.android.heysports.util
 
 import android.view.View
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
+import com.example.android.heysports.R
 import com.example.android.heysports.util.extension.clipCornerToRound
 
 /**
@@ -12,4 +15,13 @@ import com.example.android.heysports.util.extension.clipCornerToRound
 fun clipCornerToRound(layout: View, radius: Float) {
     layout.clipToOutline = true
     layout.clipCornerToRound(radius)
+}
+
+@BindingAdapter("setImageUri")
+fun ImageView.setImageUri(uri: String?) {
+    Glide.with(context)
+        .load(uri)
+        .placeholder(R.drawable.ic_launcher_background)
+        .error(R.drawable.ic_launcher_background)
+        .into(this)
 }
